7857e241a0dafe5f3d13a8f69231186fc42fb5eb
Simon1PL <saicxs123@gmail.com>
2019-05-01 15:29:47 +0200

    modules work well

--- src/main.F90
+++ src/main.F90
@@ -4,7 +4,7 @@ program main
 	use dotmath
 	implicit none
 	real(kind=4)::a(3,2), b(2,4)
-	a=[[1,2],[1,3]]
-	b=[[2,3],[3,4],[1,4],[2,5]]
+	a=reshape([1,2,1,3,1,2],[3,2])
+	b=reshape([2,3,3,4,1,4,2,5],[2,4])
 	write(*,*) naivmull(a,b), bettmull(a,b), dotmull(a,b)
 end program main
